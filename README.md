ffcaafc8b5b3e48f8a6660890af82704bef17338
Syam <syam@engravgroup.com>
2020-12-10 16:33:39 +0400

    Version 1.0.0 Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# SO Helper
+A set of helper classes for Vaadin related development. It is currently being used in the following projects.
+
+(1) https://github.com/syampillai/SOComponents  
+(2) https://github.com/syampillai/SOCharts
---
+++ src/main/java/com/storedobject/helper/ID.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright 2019-2020 Syam Pillai
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.storedobject.helper;
+
+import com.vaadin.flow.component.Component;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * A unique id generator that can be used to set a unique id for components.
+ *
+ * @author Syam
+ */
+public class ID {
+
+    private static final AtomicLong ID = new AtomicLong(1L);
+
+    private ID() {
+    }
+
+    /**
+     * Get a new id.
+     *
+     * @return New unique id.
+     */
+    public static long newID() {
+        if(ID.get() == Long.MAX_VALUE) {
+            ID.set(1);
+        }
+        return ID.getAndIncrement();
+    }
+
+    /**
+     * Set a unique id for the given component if it doesn't have an id now.
+     * (Id set will be of the format "soN" where N will be a unique long number).
+     *
+     * @param component Component for which id needs to be set.
+     */
+    public static void set(Component component) {
+        if(!component.getId().isPresent()) {
+            component.setId("so" + newID());
+        }
+    }
+}
